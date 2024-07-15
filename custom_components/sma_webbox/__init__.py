8b93b2c36a2702c2323c4e08898e42e21f6bc67c
jpcornil-git <40644331+jpcornil-git@users.noreply.github.com>
2024-07-15 20:33:16 +0200

    Replace deprecated async_forward_entry_setup call
    https://developers.home-assistant.io/blog/2024/06/12/async_forward_entry_setups/

--- custom_components/sma_webbox/__init__.py
+++ custom_components/sma_webbox/__init__.py
@@ -134,7 +134,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         SMA_WEBBOX_REMOVE_LISTENER: remove_stop_listener,
     }
 
-    await hass.config_entries.async_forward_entry_setup(entry, Platform.SENSOR)
+    await hass.config_entries.async_forward_entry_setups(entry, [Platform.SENSOR])
 
     return True
 
